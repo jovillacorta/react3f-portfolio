ace4dad4da301a6d497daef1b3a94c446ea81697
jovillacorta <jvill055@ucr.edu>
2022-12-21 23:00:34 -0800

    added text, shadows, mouse controls

--- src/components/experience/index.jsx
+++ src/components/experience/index.jsx
@@ -1,5 +1,5 @@
 
-import { Environment, Float, PresentationControls } from '@react-three/drei'
+import { Environment, Float, PresentationControls, ContactShadows, Text } from '@react-three/drei'
 import { useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
@@ -11,17 +11,41 @@ export default function Experience()
     console.log(computer);
 
     return <>
-        <Environment preset='city' />
+        <Environment preset='sunset' />
 
         <color args={ [ '#6bbcbb' ] } attach='background' />
 
-        <Float rotationIntensity={ 0.4 }>
-            <primitive object={computer.scene}
-            position-x = { -1.5 }
-            position-y = {1.5}
-            />
-        </Float>
+        <PresentationControls
+            global
+            rotation= { [ 0, 0, 0 ] }
+            polar= { [ - 1.3, .2 ] }
+            config= { { mass: 2, tension: 200 } }
+            snap = { {mass: 2, tension: 50 } }
+        >
+            <Float rotationIntensity={ 0.4 }>
+                <primitive object={computer.scene}
+                position-x = { -1.5 }
+                position-y = {1.5}
+                />
+            </Float>
+            <Text
+                font='/../../press-start-2p-v14-latin-regular.woff'
+                fontSize={ 1.1 }
+                position={ [ 3.5, 2, 3 ] }
+                rotation-x={ .1 }
+                rotation-y={ -1.5 }
+                maxWidth={ 2 }
+                textAlign= 'center'
+            > 
+                jo creates
+            </Text>
+        </PresentationControls>
         
+        <ContactShadows position-y={ - 4.75 }
+        opacity = { .5 }
+        scale = { 15 } 
+        blur = { 2.4}
+        />
 
 
     </>
